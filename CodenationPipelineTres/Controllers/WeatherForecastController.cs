1eb5af8f77957fa4f7c196c1f402387513eb9da2
Alessandra Santos <alessandradesenvolvedora@gmail.com>
2020-07-25 11:27:30 -0300

    Atualização dados Controller WeatherForescast

--- CodenationPipelineTres/Controllers/WeatherForecastController.cs
+++ CodenationPipelineTres/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace CodenationPipelineTres.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Primavera", "Verão", "Outuno", "Inverno"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
